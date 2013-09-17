f94df9c6874f3fe5786ab2c8793a8c9e67e9bd4d
JustinCWolfe <jcw_222@yahoo.com>
2013-09-17 18:19:18 +0000

    Added step wise processing base classes.

---
+++ base/src/com/messaggi/StepBase.java
@@ -0,0 +1,34 @@
+package com.messaggi;
+
+import java.util.Observable;
+
+public abstract class StepBase<T, U> extends Observable
+{
+    public enum Result {
+        Continue, Stop;
+    }
+
+    private T statistics;
+    
+    public T getStatistics()
+    {
+        return statistics;
+    }
+
+    public boolean getStepWasProcessed()
+    {
+        return statistics != null;
+    }
+
+    public abstract Result process(U data);
+
+    @Override
+    public void notifyObservers(Object arg)
+    {
+        // Toggle the changed flag for this object so that the base notifyObservers
+        // will propagate our message to observers.
+        setChanged();
+        super.notifyObservers(arg);
+    }
+}
+
