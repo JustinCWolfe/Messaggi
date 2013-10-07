57e48a70522a5cda67dca16ac5038172ff2f541c
jwolfe <jwolfe@76dc5d28-08a5-4dad-945c-d947f1181b29>
2013-10-07 17:26:17 +0000

    Work in progress for testing domain objects.

--- trunk/domain/test/com/messaggi/domain/TestDomainObjects.java
+++ trunk/domain/test/com/messaggi/domain/TestDomainObjects.java
@@ -1,16 +1,17 @@
 package com.messaggi.domain;
 
-import static org.junit.Assert.fail;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestDomainObjects
 {
-
     @BeforeClass
     public static void setUpBeforeClass() throws Exception
     {
@@ -32,10 +33,253 @@ public class TestDomainObjects
     }
 
     @Test
-    public void test()
+    public void testApplication()
     {
-        fail("Not yet implemented");
+        Application a1 = new Application();
+        Assert.assertEquals(0, a1.getId());
+        Assert.assertEquals(false, a1.isActive());
+        Assert.assertEquals(0, a1.getApplicationPlatforms().size());
+        Assert.assertNull(a1.getUserApplication());
+
+        long id = 123;
+        boolean active = true;
+        Application a2 = new Application(id, active);
+        Assert.assertEquals(id, a2.getId());
+        Assert.assertEquals(active, a2.isActive());
+        Assert.assertEquals(0, a2.getApplicationPlatforms().size());
+        Assert.assertNull(a2.getUserApplication());
+
+        Set<ApplicationPlatform> aps = new HashSet<>();
+        ApplicationPlatform ap = new ApplicationPlatform();
+        aps.add(ap);
+        UserApplication up = new UserApplication();
+        Application a3 = new Application(id, active, aps, up);
+        Assert.assertEquals(id, a3.getId());
+        Assert.assertEquals(active, a3.isActive());
+        Assert.assertEquals(1, a3.getApplicationPlatforms().size());
+        Assert.assertSame(ap, a3.getApplicationPlatforms().toArray()[0]);
+        Assert.assertSame(up, a3.getUserApplication());
     }
 
+    @Test
+    public void testApplicationPlatform()
+    {
+        ApplicationPlatform ap1 = new ApplicationPlatform();
+        Assert.assertEquals(0, ap1.getId());
+        Assert.assertNull(ap1.getPlatform());
+        Assert.assertNull(ap1.getApplication());
+        Assert.assertNull(ap1.getToken());
+
+        long id = 123;
+        Platform p = new Platform();
+        Application a = new Application();
+        String t = "Some token";
+        ApplicationPlatform ap2 = new ApplicationPlatform(id, p, a, t);
+        Assert.assertEquals(id, ap2.getId());
+        Assert.assertSame(p, ap2.getPlatform());
+        Assert.assertSame(a, ap2.getApplication());
+        Assert.assertSame(t, ap2.getToken());
+        Assert.assertEquals(0, ap2.getApplicationPlatformMsgLogs().size());
+        Assert.assertEquals(0, ap2.getApplicationPlatformAttributes().size());
+
+        /*
+         * Set<ApplicationPlatform> aps = new HashSet<>(); ApplicationPlatform
+         * ap = new ApplicationPlatform(); aps.add(ap); ApplicationPlatform up =
+         * new ApplicationPlatform(); Application ap3 = new Application(id,
+         * active, aps, up); Assert.assertEquals(id, ap3.getId());
+         * Assert.assertEquals(active, ap3.isActive()); Assert.assertEquals(1,
+         * ap3.getApplicationPlatforms().size()); Assert.assertSame(ap,
+         * ap3.getApplicationPlatforms().toArray()[0]); Assert.assertSame(up,
+         * ap3.getUserApplication());
+         */
+    }
+
+    @Test
+    public void testApplicationPlatformAttribute()
+    {
+    }
+
+    @Test
+    public void testApplicationPlatformDevice()
+    {
+    }
+
+    @Test
+    public void testApplicationPlatformKey()
+    {
+    }
+
+    @Test
+    public void testApplicationPlatformMsgLog()
+    {
+    }
+
+    @Test
+    public void testDevice()
+    {
+        Device d1 = new Device();
+        Assert.assertEquals(0, d1.getId());
+        Assert.assertEquals(false, d1.isActive());
+        Assert.assertEquals(0, d1.getDeviceAttributes().size());
+
+        long id = 123;
+        boolean active = true;
+        Device d2 = new Device(id, active);
+        Assert.assertEquals(id, d2.getId());
+        Assert.assertEquals(active, d2.isActive());
+        Assert.assertEquals(0, d2.getDeviceAttributes().size());
+
+        Set<DeviceAttribute> das = new HashSet<>();
+        DeviceAttribute da = new DeviceAttribute();
+        das.add(da);
+        Device d3 = new Device(id, active, das);
+        Assert.assertEquals(id, d3.getId());
+        Assert.assertEquals(active, d3.isActive());
+        Assert.assertEquals(1, d3.getDeviceAttributes().size());
+        Assert.assertSame(da, d3.getDeviceAttributes().toArray()[0]);
+    }
+
+    @Test
+    public void testDeviceAttribute()
+    {
+        /*
+         * DeviceAttribute da1 = new DeviceAttribute(); Assert.assertEquals(0,
+         * da1.getId()); Assert.assertEquals(false, da1.isActive());
+         * Assert.assertEquals(0, da1.getDeviceAttributes().size());
+         * 
+         * long id = 123; boolean active = true; DeviceAttribute da2 = new
+         * DeviceAttribute(id, active); Assert.assertEquals(serviceName,
+         * p2.getServiceName()); Assert.assertEquals(0,
+         * da2.getDeviceAttributes().size());
+         * 
+         * Set<DeviceAttribute> das = new HashSet<>(); DeviceAttribute da = new
+         * DeviceAttribute(); das.add(da); DeviceAttribute da3 = new
+         * DeviceAttribute(id, active, das); Assert.assertEquals(id,
+         * da3.getId()); Assert.assertEquals(active, da3.isActive());
+         * Assert.assertEquals(1, da3.getDeviceAttributes().size());
+         * Assert.assertSame(da, da3.getDeviceAttributes().toArray()[0]);
+         */
+    }
+
+    @Test
+    public void testDeviceKey()
+    {
+        DeviceKey dk1 = new DeviceKey();
+        Assert.assertNull(dk1.getKey());
+        Assert.assertNull(dk1.getDescription());
+        Assert.assertEquals(0, dk1.getDeviceAttributes().size());
+
+        String key = "BUNDLE_ID";
+        String description = "Apple Application Bundle Identifier";
+        DeviceKey dk2 = new DeviceKey(key, description);
+        Assert.assertEquals(key, dk2.getKey());
+        Assert.assertEquals(description, dk2.getDescription());
+        Assert.assertEquals(0, dk2.getDeviceAttributes().size());
+
+        Set<DeviceAttribute> das = new HashSet<>();
+        DeviceAttribute da = new DeviceAttribute();
+        das.add(da);
+        DeviceKey dk3 = new DeviceKey(key, description, das);
+        Assert.assertEquals(key, dk3.getKey());
+        Assert.assertEquals(description, dk3.getDescription());
+        Assert.assertEquals(1, dk3.getDeviceAttributes().size());
+        Assert.assertSame(da, dk3.getDeviceAttributes().toArray()[0]);
+    }
+
+    @Test
+    public void testPlatform()
+    {
+        Platform p1 = new Platform();
+        Assert.assertEquals(0, p1.getId());
+        Assert.assertNull(p1.getName());
+        Assert.assertNull(p1.getServiceName());
+        Assert.assertEquals(false, p1.isActive());
+        Assert.assertEquals(0, p1.getApplicationPlatforms().size());
+
+        long id = 123;
+        boolean active = true;
+        String name = "iOS";
+        String serviceName = "Apple Push Notification Service";
+        Platform p2 = new Platform(id, name, serviceName, active);
+        Assert.assertEquals(id, p2.getId());
+        Assert.assertEquals(name, p2.getName());
+        Assert.assertEquals(serviceName, p2.getServiceName());
+        Assert.assertEquals(active, p2.isActive());
+        Assert.assertEquals(0, p2.getApplicationPlatforms().size());
+
+        Set<ApplicationPlatform> aps = new HashSet<>();
+        ApplicationPlatform ap = new ApplicationPlatform();
+        aps.add(ap);
+        Platform p3 = new Platform(id, name, serviceName, active, aps);
+        Assert.assertEquals(id, p3.getId());
+        Assert.assertEquals(name, p3.getName());
+        Assert.assertEquals(serviceName, p3.getServiceName());
+        Assert.assertEquals(active, p3.isActive());
+        Assert.assertEquals(1, p3.getApplicationPlatforms().size());
+        Assert.assertSame(ap, p3.getApplicationPlatforms().toArray()[0]);
+    }
+
+    @Test
+    public void testUser()
+    {
+        User u1 = new User();
+        Assert.assertEquals(0, u1.getId());
+        Assert.assertNull(u1.getName());
+        Assert.assertNull(u1.getEmail());
+        Assert.assertNull(u1.getPhone());
+        Assert.assertNull(u1.getPhoneParsed());
+        Assert.assertNull(u1.getPassword());
+        Assert.assertNull(u1.getLocale());
+        Assert.assertEquals(false, u1.isActive());
+        Assert.assertEquals(0, u1.getUserApplications().size());
+
+        long id = 123;
+        boolean active = true;
+        String name = "Justin C. Wolfe";
+        String email = "Justin@Messaggi.com";
+        String phone = "617-549-1234";
+        String phoneParsed = "6175491234";
+        String password = "123abXX!!";
+        String locale = "en-US";
+        User u2 = new User(id, name, email, phone, phoneParsed, password, locale, active);
+        Assert.assertEquals(id, u2.getId());
+        Assert.assertEquals(name, u2.getName());
+        Assert.assertEquals(email, u2.getEmail());
+        Assert.assertEquals(phone, u2.getPhone());
+        Assert.assertEquals(phoneParsed, u2.getPhoneParsed());
+        Assert.assertEquals(password, u2.getPassword());
+        Assert.assertEquals(locale, u2.getLocale());
+        Assert.assertEquals(active, u2.isActive());
+        Assert.assertEquals(0, u2.getUserApplications().size());
+
+        Set<UserApplication> uas = new HashSet<>();
+        UserApplication ua = new UserApplication();
+        uas.add(ua);
+        User u3 = new User(id, name, email, phone, phoneParsed, password, locale, active, uas);
+        Assert.assertEquals(id, u3.getId());
+        Assert.assertEquals(name, u3.getName());
+        Assert.assertEquals(email, u3.getEmail());
+        Assert.assertEquals(phone, u3.getPhone());
+        Assert.assertEquals(phoneParsed, u3.getPhoneParsed());
+        Assert.assertEquals(password, u3.getPassword());
+        Assert.assertEquals(locale, u3.getLocale());
+        Assert.assertEquals(active, u3.isActive());
+        Assert.assertEquals(1, u3.getUserApplications().size());
+        Assert.assertSame(ua, u3.getUserApplications().toArray()[0]);
+    }
+
+    @Test
+    public void testUserApplication()
+    {
+        UserApplication ua1 = new UserApplication();
+        Assert.assertEquals(0, ua1.getApplicationId());
+        Assert.assertNull(ua1.getUser());
+
+        long applicationId = 123;
+        User user = new User();
+        UserApplication ua2 = new UserApplication(applicationId, user);
+        Assert.assertEquals(applicationId, ua2.getApplicationId());
+        Assert.assertSame(user, ua2.getUser());
+    }
 }
 
