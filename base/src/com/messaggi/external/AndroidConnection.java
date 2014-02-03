af489a638886890bed03d116847367333964d6ab
JustinCWolfe <jcw_222@yahoo.com>
2014-02-03 21:45:06 +0000

    send message wip

--- base/src/com/messaggi/external/AndroidConnection.java
+++ base/src/com/messaggi/external/AndroidConnection.java
@@ -2,6 +2,7 @@ package com.messaggi.external;
 
 import com.messaggi.domain.ApplicationPlatform;
 import com.messaggi.messages.SendMessageRequest;
+import com.messaggi.messages.SendMessageResponse;
 
 public class AndroidConnection implements MessagingServiceConnection
 {
@@ -26,9 +27,9 @@ public class AndroidConnection implements MessagingServiceConnection
     }
 
     @Override
-    public boolean sendMessage(SendMessageRequest request)
+    public SendMessageResponse sendMessage(SendMessageRequest request)
     {
-        return false;
+        return new SendMessageResponse();
     }
 }
 
