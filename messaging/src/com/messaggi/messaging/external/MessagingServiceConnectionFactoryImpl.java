e0540309c36523fb911ccfdb31e464e0ba34049b
JustinCWolfe <jcw_222@yahoo.com>
2014-02-07 20:25:30 +0000

    fixed message service connection factory

--- messaging/src/com/messaggi/messaging/external/MessagingServiceConnectionFactoryImpl.java
+++ messaging/src/com/messaggi/messaging/external/MessagingServiceConnectionFactoryImpl.java
@@ -2,10 +2,12 @@ package com.messaggi.messaging.external;
 
 import com.messaggi.domain.ApplicationPlatform;
 import com.messaggi.external.MessagingServiceConnection;
+import com.messaggi.external.MessagingServiceConnectionFactory;
 
-public class MessagingServiceConnectionFactory
+public class MessagingServiceConnectionFactoryImpl implements MessagingServiceConnectionFactory
 {
-    public static MessagingServiceConnection create(ApplicationPlatform applicationPlatform)
+    @Override
+    public MessagingServiceConnection create(ApplicationPlatform applicationPlatform)
     {
         MessagingServiceConnection messagingConnection = null;
         switch (applicationPlatform.getPlatform()) {
