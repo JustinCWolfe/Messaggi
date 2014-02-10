be3a7aee79e6223b986dfedf0328a5927e00c75d
jwolfe <jwolfe@76dc5d28-08a5-4dad-945c-d947f1181b29>
2014-02-10 17:23:04 +0000

    working android message send

--- trunk/messaging/test/com/messaggi/messaging/external/TestAndroidConnection.java
+++ trunk/messaging/test/com/messaggi/messaging/external/TestAndroidConnection.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertSame;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.messaggi.TestDataHelper.ApplicationPlatform1;
 import com.messaggi.TestDataHelper.ApplicationPlatformAndroidTesting;
 import com.messaggi.TestDataHelper.Device1;
 import com.messaggi.TestDataHelper.Device2;
@@ -18,7 +19,6 @@ import com.messaggi.external.MessagingServiceConnection;
 import com.messaggi.external.MessagingServiceConnectionFactory;
 import com.messaggi.junit.MessaggiTestCase;
 import com.messaggi.messages.SendMessageRequest;
-import com.messaggi.util.JAXBHelper;
 
 public class TestAndroidConnection extends MessaggiTestCase
 {
@@ -59,36 +59,36 @@ public class TestAndroidConnection extends MessaggiTestCase
     @Test
     public void testSendMessage_InvalidAPIKey() throws Exception
     {
-
+        Device[] to = { D2 };
+        ApplicationPlatform invalidAPIKeyAppPlat = ApplicationPlatform1.getDomainObject();
+        MessagingServiceConnection connection = MessagingServiceConnectionFactory.Instance.getInstance().create(
+                invalidAPIKeyAppPlat);
+        SendMessageRequest smr = new SendMessageRequest(D1, to, MESSAGE1_TEXT, true);
+        connection.sendMessage(smr);
     }
 
     @Test
     public void testSendMessage_InvalidRegistrationIDKey() throws Exception
     {
-
+        Device[] to = { D2 };
+        SendMessageRequest smr = new SendMessageRequest(D1, to, MESSAGE1_TEXT, true);
+        androidConnection.sendMessage(smr);
     }
 
     @Test
-    public void testSendMessage() throws Exception
+    public void testSendMessage_SingleRegistrationID() throws Exception
     {
-        //Content-Type:application/json
-        //Authorization:key=AIzaSyB-1uEai2WiUapxCs2Q0GZYzPu7Udno5aA
-        //{
-        //  "registration_ids" : ["APA91bHun4MxP5egoKMwt2KZFBaFUH-1RYqx..."],
-        //  "data" : {
-        //    ...
-        //  },
-        //}
-        Device[] to = { D2, D3 };
+        Device[] to = { D2 };
         SendMessageRequest smr = new SendMessageRequest(D1, to, MESSAGE1_TEXT, true);
-        System.out.println(JAXBHelper.objectToXML(smr));
         androidConnection.sendMessage(smr);
     }
 
     @Test
     public void testSendMessage_MultipleRegistrationIDs() throws Exception
     {
-
+        Device[] to = { D2, D3, D4, D5, D6 };
+        SendMessageRequest smr = new SendMessageRequest(D1, to, MESSAGE1_TEXT, true);
+        androidConnection.sendMessage(smr);
     }
 }
 
