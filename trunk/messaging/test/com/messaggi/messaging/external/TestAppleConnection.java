bf22746bb66005f0e6ee0080d6868ef2ab066130
jwolfe <jwolfe@76dc5d28-08a5-4dad-945c-d947f1181b29>
2014-02-21 20:24:15 +0000

    apple connection wip

--- trunk/messaging/test/com/messaggi/messaging/external/TestAppleConnection.java
+++ trunk/messaging/test/com/messaggi/messaging/external/TestAppleConnection.java
@@ -1,5 +1,7 @@
 package com.messaggi.messaging.external;
 
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 import org.junit.After;
@@ -8,12 +10,22 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestAppleConnection
+import com.messaggi.TestDataHelper.ApplicationPlatformAppleTesting;
+import com.messaggi.domain.ApplicationPlatform;
+import com.messaggi.external.MessagingServiceConnection;
+import com.messaggi.external.MessagingServiceConnectionFactory;
+import com.messaggi.junit.MessaggiTestCase;
+
+public class TestAppleConnection extends MessaggiTestCase
 {
+    private static MessagingServiceConnection appleConnection;
+
+    private static final ApplicationPlatform APP_PLAT = ApplicationPlatformAppleTesting.getDomainObject();
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception
     {
+        appleConnection = new MockAppleConnection();
     }
 
     @AfterClass
@@ -21,14 +33,23 @@ public class TestAppleConnection
     {
     }
 
+    @Override
     @Before
     public void setUp() throws Exception
     {
     }
 
+    @Override
     @After
     public void tearDown() throws Exception
     {
+    }
+
+    @Test
+    public void testMessagingServiceConnectionFactory() throws Exception
+    {
+        MessagingServiceConnection conn = MessagingServiceConnectionFactory.Instance.getInstance().create(APP_PLAT);
+        assertThat(APP_PLAT, sameInstance(conn.getApplicationPlatform()));
     }
 
     @Test
@@ -36,6 +57,5 @@ public class TestAppleConnection
     {
         fail("Not yet implemented");
     }
-
 }
 
