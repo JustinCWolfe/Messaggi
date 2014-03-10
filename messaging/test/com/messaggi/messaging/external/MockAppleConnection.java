5a6891c4cc0f0798855a963ab0c29c52630dc371
JustinCWolfe <jcw_222@yahoo.com>
2014-03-10 19:40:38 +0000

    apple connect wip

--- messaging/test/com/messaggi/messaging/external/MockAppleConnection.java
+++ messaging/test/com/messaggi/messaging/external/MockAppleConnection.java
@@ -1,6 +1,13 @@
 package com.messaggi.messaging.external;
 
+import com.notnoop.apns.ApnsServiceBuilder;
+
 public class MockAppleConnection extends AppleConnection
 {
+    @Override
+    protected void setBuilderDestination(ApnsServiceBuilder builder)
+    {
+        builder.withSandboxDestination();
+    }
 }
 
