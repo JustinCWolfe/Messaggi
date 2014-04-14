45951cca842e1011e2dd85e9ac6ea2eb8f38669d
jwolfe <jwolfe@76dc5d28-08a5-4dad-945c-d947f1181b29>
2014-04-14 14:42:30 +0000

    Finished task transfer.

--- trunk/base/src/com/messaggi/pool/AutoResizingThreadPool.java
+++ trunk/base/src/com/messaggi/pool/AutoResizingThreadPool.java
@@ -1,5 +1,7 @@
 package com.messaggi.pool;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
@@ -21,6 +23,10 @@ public class AutoResizingThreadPool extends ThreadPool
 
     private static final int SCHEDULED_SERVICE_POOL_SIZE = 2;
 
+    private final Object queueToPoolTransferLock;
+
+    private final AtomicBoolean queueToPoolTransferComplete;
+
     private final AtomicBoolean resizing;
 
     private final BlockingDeque<Task<?>> resizingTemporaryTaskQueue;
@@ -30,6 +36,8 @@ public class AutoResizingThreadPool extends ThreadPool
     public AutoResizingThreadPool()
     {
         super();
+        queueToPoolTransferLock = new Object();
+        queueToPoolTransferComplete = new AtomicBoolean();
         resizing = new AtomicBoolean();
         resizingTemporaryTaskQueue = new LinkedBlockingDeque<>();
         scheduledExecutorService = Executors.newScheduledThreadPool(SCHEDULED_SERVICE_POOL_SIZE);
@@ -41,14 +49,22 @@ public class AutoResizingThreadPool extends ThreadPool
     protected void addTaskInternal(Task<?> task, boolean addToFront)
     {
         if (isResizing()) {
-            if (addToFront) {
-                resizingTemporaryTaskQueue.addFirst(task);
-            } else {
-                resizingTemporaryTaskQueue.addLast(task);
+            // Resizing is underway and we would be able to acquire the lock if either:
+            // 1. task transfer from temporary queue to pool is not yet underway
+            // 2. task transfer from temporary queue to pool is complete
+            // Only add to temporary queue in the first case.
+            synchronized (queueToPoolTransferLock) {
+                if (!queueToPoolTransferComplete.get()) {
+                    if (addToFront) {
+                        resizingTemporaryTaskQueue.addFirst(task);
+                    } else {
+                        resizingTemporaryTaskQueue.addLast(task);
+                    }
+                    return;
+                }
             }
-        } else {
-            super.addTaskInternal(task, addToFront);
         }
+        super.addTaskInternal(task, addToFront);
     }
 
     private boolean isResizing()
@@ -73,26 +89,28 @@ public class AutoResizingThreadPool extends ThreadPool
 
     private void resize(int newThreadCount)
     {
-        resizing.set(true);
-        if (resizingTemporaryTaskQueue.size() > 0) {
-            throw new RuntimeException("Tasks remaining on resizing queue: " + resizingTemporaryTaskQueue.size());
-        }
-        // Do not shut down the scheduled executor service - just poison the pool threads (so we
-        // call super.shutdown instead of shutdown).
-        super.shutdown();
-        for (;;) {
-            System.out.println("Tasks remaining: " + getPoolTaskCount());
-            try {
-                if (awaitTermination(SECONDS_BETWEEN_POOL_AWAIT_TERMINATION_CALLS, TimeUnit.SECONDS)) {
-                    break;
-                }
-            } catch (InterruptedException e) {
-
+        if (resizing.compareAndSet(false, true)) {
+            queueToPoolTransferComplete.set(false);
+            if (resizingTemporaryTaskQueue.size() > 0) {
+                throw new RuntimeException("Tasks remaining on resizing queue: " + resizingTemporaryTaskQueue.size());
             }
+            // Do not shut down the scheduled executor service - just poison the pool threads (so we
+            // call super.shutdown instead of shutdown).
+            super.shutdown();
+            for (;;) {
+                System.out.println("Tasks remaining: " + getPoolTaskCount());
+                try {
+                    if (awaitTermination(SECONDS_BETWEEN_POOL_AWAIT_TERMINATION_CALLS, TimeUnit.SECONDS)) {
+                        break;
+                    }
+                } catch (InterruptedException e) {
+
+                }
+            }
+            initialize(newThreadCount);
+            transferTasksFromTemporaryQueueToPool();
+            resizing.set(false);
         }
-        initialize(newThreadCount);
-        //TODO: Transfer temporary tasks to new pool task queues.
-        resizing.set(false);
     }
 
     @Override
@@ -100,6 +118,18 @@ public class AutoResizingThreadPool extends ThreadPool
     {
         scheduledExecutorService.shutdown();
         super.shutdown();
+    }
+
+    private void transferTasksFromTemporaryQueueToPool()
+    {
+        synchronized (queueToPoolTransferLock) {
+            List<Task<?>> temporaryTasks = new ArrayList<>(resizingTemporaryTaskQueue.size());
+            resizingTemporaryTaskQueue.drainTo(temporaryTasks);
+            for (Task<?> task : temporaryTasks) {
+                super.addTaskInternal(task, false);
+            }
+            queueToPoolTransferComplete.set(true);
+        }
     }
 
     private static class ScheduledInspectPoolQueueSizeTask implements Runnable
